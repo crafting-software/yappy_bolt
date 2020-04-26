3b6e8eae06d14d58bc6fff997843815ad767f9f3
constantin.cheptea <cheptea.constantin@gmail.com>
2020-04-26 12:42:45 +0300

    Implemented OAuth. Saving workspace data to db. Test scheduled function to send messages.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 'use strict'
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const rp = require("request-promise");
 const config = functions.config();
 
 const { App, ExpressReceiver } = require('@slack/bolt');
@@ -16,11 +18,19 @@ const app = new App({
     token: config.slack.bot_token
 });
 
+var serviceAccount = require("./yappy_service_account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://yappy-cd44b.firebaseio.com"
+});
+
 // Global error handler
 app.error(console.log);
 // Handle `/echo` command invocations
-app.command('/echo', async ({ command, ack, say }) => {
+app.command('/echo-from-firebase', async ({ command, ack, say }) => {
   // Acknowledge command request
+  console.log(functions.config().firebase)
   console.log('Requesting Echo Test');
   await ack();
 
@@ -70,8 +80,97 @@ function broadcastMessage(usersList, message) {
   })
 }
 
-// https://{your domain}.cloudfunctions.net/slack/events
-// exports.slack = functions.https.onRequest(app);
 exports.slack = functions.https.onRequest(async (req, res) => {
-    expressReceiver.app(req, res);
+  expressReceiver.app(req, res);
+});
+
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+  console.log('every 1 minutes!');
+  sendMessagesToWorkspaces();
+  return null;
+});
+
+exports.test = functions.https.onRequest(async (request, response) => {
+
+  sendMessagesToWorkspaces();
+  response.send("Test ok.")
+});
+
+async function sendMessagesToWorkspaces(){
+  console.log("Test");
+
+  admin.database().ref("installations")
+
+  var db = admin.database();
+  var ref = db.ref("installations");
+  await ref.once("value", function(snapshot) {
+
+    snapshot.forEach(function(data) {
+      let workspace = data.val();
+      console.log(`Sending message to ${workspace.team.name} -> ${workspace.webhook.channel}`);
+      const result = app.client.chat.postMessage({
+        token: workspace.token,
+        channel: workspace.webhook.channel_id,
+        user: workspace.bot_user_id,
+        text: "This is using the database"
+      });
+      console.log(`Sent message to ${workspace.team.name} -> ${workspace.webhook.channel}`);
+    });
+
+  });
+}
+
+exports.oauth = functions.https.onRequest(async (request, response) => {
+  console.log("Requested OAuth Flow...")
+
+  if (request.method !== "GET") {
+    console.error(`Got unsupported ${request.method} request. Expected GET.`);
+    return response.send(405, "Only GET requests are accepted");
+  }
+
+  console.log("Checking params...")
+
+  if (!request.query && !request.query.code) {
+      return response.status(401).send("Missing query attribute 'code'");
+  }
+
+  console.log("Request is ok, initiating handshake...")
+
+  const options = {
+    uri: "https://slack.com/api/oauth.v2.access",
+    method: "GET",
+    json: true,
+    qs: {
+      code: request.query.code,
+      client_id: functions.config().slack.client_id,
+      client_secret: functions.config().slack.client_secret,
+      redirect_uri: "https://5af759f7.ngrok.io/yappy-cd44b/us-central1/oauth"
+    }
+  };
+
+  const result = await rp(options);
+  if (!result.ok) {
+    console.error("The request was not ok: " + JSON.stringify(result));
+    return response.header("Location", 'https://yappy-cd44b.web.app').send(302);
+  }
+
+  console.log("OAuth Success!")
+  console.log(result)
+
+  await admin.database().ref("installations").child(result.team.id).set({
+    token: result.access_token,
+    team: {
+      id: result.team.id,
+      name: result.team.name
+    },
+    bot_user_id: result.bot_user_id,
+    webhook: {
+        url: result.incoming_webhook.url,
+        channel: result.incoming_webhook.channel,
+        channel_id: result.incoming_webhook.channel_id
+    }
+  });
+
+  console.log("A new workspace installed Yappy!")
+  response.header("Location", 'https://yappy-cd44b.web.app/success.html').send(302);
 });
