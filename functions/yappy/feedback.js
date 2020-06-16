8d42555ebea61de00612effe2dd1f3223dd05ecb
Salajan Dan <salajan.dan11@gmail.com>
2020-06-16 16:31:41 +0300

    added timestamp to feedback entries

--- functions/yappy/feedback.js
+++ functions/yappy/feedback.js
@@ -1,9 +1,14 @@
 const admin = require("firebase-admin");
+const moment = require("moment");
+const { v4 } = require("uuid");
 
 async function sendFeedback(app, { body, context, view, ack }) {
   await ack();
   const message = view.state.values.feedback_message.feedback_input.value;
-  await admin.database().ref(`feedback/${body.team.id}`).push(message);
+  await admin
+    .database()
+    .ref(`feedback/${body.team.id}/${v4()}`)
+    .set({ ts: moment.utc().unix(), message: message });
 
   await app.client.chat.postMessage({
     token: context.botToken,
